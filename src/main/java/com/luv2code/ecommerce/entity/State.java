0cec558d7233768712a24457c5644e7ef31b6d35
Mayank Singh <mayanksingh56643@gmail.com>
2025-01-08 21:31:41 +0530

    Version 1 for the Product

---
+++ src/main/java/com/luv2code/ecommerce/entity/State.java
@@ -0,0 +1,22 @@
+package com.luv2code.ecommerce.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="state")
+@Data
+public class State {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+@ManyToOne
+@JoinColumn(name = "country")
+    private Country country;
+}
